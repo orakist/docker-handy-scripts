07ab8a5eded82c7ab466a5d9e656e93c1bfd8ea5
Orhan Ak <52536669+orakist@users.noreply.github.com>
2022-05-27 16:57:56 +0100

    Update docker-volume-cloner.ps1

--- docker-volume-cloner.ps1
+++ docker-volume-cloner.ps1
@@ -1,11 +1,13 @@
-#This is a script that can help to easily create a clone of a given data volume.
-#The script is mainly useful if you are using named volumes
-#Example usage: .\docker-volume-cloner.ps1 volume-data-1 volume-data-2
+# Author: Orhan Ak
 
-#Get source and destination parameters 
+# This is a windows powershell script that can help to easily create a clone of a 
+# given data volume. The script can be very useful if you are using named volumes.
+# Example usage: .\docker-volume-cloner.ps1 volume-data-1 volume-data-2
+
+# Get source and destination parameters 
 param( [string]$src, [string]$dest )
 
-#First check if the user provided all needed arguments
+# First check if the user provided all needed arguments
 if ( $src.length -lt 5 -or $dest.length -lt 5 )
 {
 	echo "Please provide valid source volume name and destination volume name!"
@@ -18,7 +20,7 @@ if ( $src -eq $dest )
 	exit
 }
  
-#Check if the source volume name does exist
+# Check if the source volume name does exist
 docker volume inspect $src > null 2>&1
 if ( !$? )
 {
@@ -26,7 +28,7 @@ if ( !$? )
 	exit
 }
 
-#check if the destination volume name does not exist
+# check if the destination volume name does not exist
 docker volume inspect $dest > null 2>&1
 if ( $? )
 {
@@ -37,5 +39,5 @@ if ( $? )
 echo "Creating destination volume $dest ..."
 docker volume create --name $dest
 
-echo "Copying data from source volume ($src) to destination volume ($dest)..."
+echo "Copying data from source volume ($src) to destination volume ($dest) ..."
 docker run --rm -i -t -v ${src}:/from -v ${dest}:/to alpine ash -c "cd /from ; cp -av . /to"
